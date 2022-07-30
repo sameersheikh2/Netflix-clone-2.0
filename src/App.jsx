c9c9fa10fd091debed99bd4a9c1d465652933630
sameersheikh2 <sameersheikh819@gmail.com>
2022-07-30 16:50:51 +0530

    initialize with create-react-app

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./App.scss"
+import Home from './home/Home'
+
+const App = () => {
+  return (
+    <div><Home/></div>
+  )
+}
+
+export default App
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <div className="container">
+            <div className="left">
+                <img  src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png" alt="" />
+            </div>
+            <div className="right"></div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
