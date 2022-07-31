1cdf8f5c45cdd007936539f3998bb1b8681e7e0d
sameersheikh2 <sameersheikh819@gmail.com>
2022-07-31 20:42:41 +0530

    created list and listItem components and styled, added some vid and img

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,3 +1,4 @@
+import List from "../components/list/List"
 import React from 'react'
 import Featured from '../components/featured/Featured'
 import Navbar from '../components/navbar/Navbar'
@@ -8,6 +9,7 @@ const Home = () => {
     <div className='home'>
       <Navbar />
       <Featured />
+      <List/>
     </div>
   )
 }
